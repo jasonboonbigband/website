eebd010a93f3defcbcf2da25a289054e9d003029
Icebell <ice-bell@gmx.ch>
2014-07-14 08:44:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 	
       Wir spielen regelmässig an Open-Airs, Festivals und privaten Anlässen. Ebenso geben wir pro Jahr 3-4 Strassekonzerte im Kanton Zürich.</p>
       <p>&nbsp;</p>
+      <p>&nbsp;</p>
     </div>
   </div>
 
