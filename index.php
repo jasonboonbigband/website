57fd69c00111b380674e8c2826b752b187c9434d
Icebell <ice-bell@gmx.ch>
2014-07-21 20:37:09 +0200

    schöneri darstellig

--- index.php
+++ index.php
@@ -18,16 +18,12 @@
       <p>Die Jason Boon Bigband besteht aus zwanzig ambitionierten Amateur- und Berufsmusikern. Wir haben ein breites Repertoire an poppigen und jazzigen Tunes, mit denen wir Ihre Geburtstags-, Hochzeits- oder Geschäftsfeier umrahmen können.
 	
       Wir spielen regelmässig an Open-Airs, Festivals und privaten Anlässen. Ebenso geben wir pro Jahr 3-4 Strassekonzerte im Kanton Zürich.</p>
+      <p>&nbsp;</p>
       
       <h2>News</h2>
       <p> Die Jason Boon Bigband ist dabei beim "Kampf der Orchester"! Wir freuen uns riesig auf diese Herausforderung. 
       Mehr Infos unter: <br /></p>
-      <a href="http://www.srf.ch/kampfderorchester" target="_blank">Kampf der Orchester</a>
-      
-      
-      <p>&nbsp;</p>
-      <br> <br> <br>
-	  
+      <a href="http://www.srf.ch/kampfderorchester" target="_blank">Kampf der Orchester</a><br> <br>
 <div style="float: right;">
 		<a href="https://www.facebook.com/JasonBoonBigBand?fref=ts" target="_blank"><img src="images/facebook.png" width="150" /></a> 
 	 
