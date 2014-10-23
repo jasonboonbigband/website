b97497c7dbf00c0ccf376b7aef08414ef399c4c2
gryphius <oli@fuglu.org>
2014-10-23 16:17:06 +0200

    kdo info  und ticket link update

--- index.php
+++ index.php
@@ -19,19 +19,19 @@
       <p>Die Jason Boon Bigband besteht aus zwanzig ambitionierten Amateur- und Berufsmusikern. Wir haben ein breites Repertoire an poppigen und jazzigen Tunes, mit denen wir Ihre Geburtstags-, Hochzeits- oder Geschäftsfeier umrahmen können.
 	
       Wir spielen regelmässig an Open-Airs, Festivals und privaten Anlässen. Ebenso geben wir pro Jahr 3-4 Strassekonzerte im Kanton Zürich.</p>
-      <p>&nbsp;</p>
+  
+      <h2>Kampf der Orchester</h2>
+      Die Jason Boon Bigband ist dabei beim <a href="http://www.srf.ch/kampfderorchester" target="_blank">Kampf der Orchester</a>! Wir freuen uns riesig auf diese Herausforderung. 
+      Unsere Erfahrungen halten wir in in unserem <a href="http://jasonboonbigband.blogspot.ch/" target="_blank">KdO-Blog</a> fest!
       
-      <h2>News</h2>
-      <p> Die Jason Boon Bigband ist dabei beim "Kampf der Orchester"! Wir freuen uns riesig auf diese Herausforderung. 
-      Mehr Infos unter: <br /></p>
-      <p><a href="http://www.srf.ch/kampfderorchester" target="_blank">&gt; Kampf der Orchester</a></p>
-      <p><a href="http://jasonboonbigband.blogspot.ch/" target="_blank">&gt; Kampf der Orchester Blog der Jbbb</a></p>
-      <p><a href="https://docs.google.com/forms/d/1EfILTbdNnSBgnW282pQP-UKd3-bXhVSN8xUmt9eM9DY/viewform?usp=send_form" target="_blank">&gt; Ticketreservation im JBBB-Fansektor f&uuml;r die Sendungen in der Bodensee-Arena</a></p>
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <p><br> 
-        <br>
-      </p>
+      <p><b>Sei live dabei!</b> Tickets (ab CHF 20.-) für die Live-Shows in der Bodensee-Arena gibt es bei <a href="https://www.starticket.ch/0showlist.asp?Language=D&txtSearch=kampf+der+orchester&suchen.x=0&suchen.y=0&fulltextHidden=1">Starticket</a></p>
+      
+      <p>Willst du dich in der Bodensee-Arena (oder wo auch immer du willst) als
+Boons-Fan zu erkennen geben? Dann gönne dir den unverwechselbaren
+Boons-Hut zu CHF 15.- Wir freuen uns auf deine Bestellung unter
+<a href="fans@jbbb.ch">fans@jbbb.ch</a>. Der Hut-gegen-Bargeld-Tausch findet dann direkt vor der
+Show statt.</p>
+      
       <a href="https://www.facebook.com/JasonBoonBigBand?fref=ts" target="_blank"><img src="images/facebook.png" width="150" align="right" /></a>
       
     </div>
