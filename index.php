e010acddf3b8f6c999dcf41057339c63e67ee0c7
Phantom75 <martin@schacher.ch>
2014-11-05 21:13:34 +0100

    rearrange

--- index.php
+++ index.php
@@ -1,38 +1,23 @@
 <?php include("header.php"); ?>
-
 <div class ="mainborder">
-
-  <div class ="header">
-  	<img src="images/header/home-header2.png" width="960" height="380" ></img>
-  </div>
-
- 
+  <div class ="header"> <img src="images/header/home-header2.png" width="960" height="380" ></img> </div>
   <div class="lowerpart">
-  
-  	<div class="silhouette" >
-		<img src="images/kopf.png" name="bottomImage" width="400" id="bottomImage" align="absbottom" />
-	</div>
-
-    
+    <div class="silhouette" > <img src="images/kopf.png" name="bottomImage" width="400" id="bottomImage" align="absbottom" /> </div>
     <div class ="content">
-    <h1>Kampf der Orchester</h1>
-      <p>Die Jason Boon Bigband ist dabei beim <a href="http://www.srf.ch/kampfderorchester" target="_blank">Kampf der Orchester</a>!
- </p>
+      <h1>Kampf der Orchester</h1>
+      <p>Die Jason Boon Bigband ist dabei beim <a href="http://www.srf.ch/kampfderorchester" target="_blank">Kampf der Orchester</a>! </p>
       <p><img src="images/boons_kdo.jpg" width="500" alt="JBBB" /> </p>
       <p>Tixx für die Live-Shows in der Bodensee-Arena:<a href="https://www.starticket.ch/0showlist.asp?Language=D&txtSearch=kampf+der+orchester&suchen.x=0&suchen.y=0&fulltextHidden=1"> Starticket</a></p>
-     <div class="fb-like-box" data-href="https://www.facebook.com/jasonboonbigband" data-width="300" data-height="500" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
-
-<a class="twitter-timeline" href="https://twitter.com/JBBB_CH" data-widget-id="530088203810656256">Tweets von @JBBB_CH</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-
+      
+      <a class="twitter-timeline" href="https://twitter.com/JBBB_CH" data-widget-id="530088203810656256">Tweets von @JBBB_CH</a> 
+      
+      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+      
       <iframe width="500" height="300" src="//www.youtube.com/embed/videoseries?list=PLilMLbHHTVTGODTlY90S9LsVLfi_a-ikB" frameborder="0" allowfullscreen></iframe>
-    </div>
+      
+   </div>
+    <div class="fb-like-box" data-href="https://www.facebook.com/jasonboonbigband" data-width="300" data-height="500" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
     <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fjasonboonbigband&amp;width=300&amp;height=590&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=true&amp;show_border=true" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:590px;" allowTransparency="true"></iframe>
-
-
   </div>
 </div>
-
-
-
 <?php include("footer.php"); ?>
