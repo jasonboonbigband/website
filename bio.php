94c41889e8a5bb185f926db4cfbfa640b03d768a
Phantom75 <martin@schacher.ch>
2014-11-05 20:40:44 +0100

    Inserted FB-Plugin
    https://developers.facebook.com/docs/plugins/like-box-for-pages

---
+++ bio.php
@@ -0,0 +1,25 @@
+<?php include("header.php"); ?>
+
+<div class ="mainborder">
+
+  <div class ="header">
+  <img src="images/header/home-header4.png" width="960" height="380" />
+  </div>
+
+  <div class="lowerpart">
+
+  <div id="silhouette">
+      <img src="images/kopf.png" width="450" id="bottomImage" />
+  </div>
+
+  <div class ="content">
+    <h1>Jason Boon Bigband</h1>
+    <p>Die Jason Boon Bigband besteht aus zwanzig ambitionierten Amateur- und Berufsmusikern. Wir haben ein breites Repertoire an poppigen und jazzigen Tunes, mit denen wir Ihre Geburtstags-, Hochzeits- oder Geschäftsfeier umrahmen können.
+      
+      Wir spielen regelmässig an Open-Airs, Festivals und privaten Anlässen. </p>
+  </div>
+  </div>
+
+</div>
+
+<?php include("footer.php"); ?>
--- index.php
+++ index.php
@@ -15,25 +15,12 @@
 
     
     <div class ="content">
-    <h1> Jason Boon Bigband</h1>
-      <p>Die Jason Boon Bigband besteht aus zwanzig ambitionierten Amateur- und Berufsmusikern. Wir haben ein breites Repertoire an poppigen und jazzigen Tunes, mit denen wir Ihre Geburtstags-, Hochzeits- oder Geschäftsfeier umrahmen können.
-	
-      Wir spielen regelmässig an Open-Airs, Festivals und privaten Anlässen. Ebenso geben wir pro Jahr 3-4 Strassekonzerte im Kanton Zürich.</p>
-  
-      <h2>Kampf der Orchester</h2>
-      Die Jason Boon Bigband ist dabei beim <a href="http://www.srf.ch/kampfderorchester" target="_blank">Kampf der Orchester</a>! Wir freuen uns riesig auf diese Herausforderung. 
-      Unsere Erfahrungen halten wir in in unserem <a href="http://jasonboonbigband.blogspot.ch/" target="_blank">KdO-Blog</a> fest!
-      
-      <p><b>Sei live dabei!</b> Tickets (ab CHF 20.-) für die Live-Shows in der Bodensee-Arena gibt es bei <a href="https://www.starticket.ch/0showlist.asp?Language=D&txtSearch=kampf+der+orchester&suchen.x=0&suchen.y=0&fulltextHidden=1">Starticket</a></p>
-      
-      <p>Willst du dich in der Bodensee-Arena (oder wo auch immer du willst) als
-Boons-Fan zu erkennen geben? Dann gönne dir den unverwechselbaren
-Boons-Hut zu CHF 15.- Wir freuen uns auf deine Bestellung unter
-<a href="fans@jbbb.ch">fans@jbbb.ch</a>. Der Hut-gegen-Bargeld-Tausch findet dann direkt vor der
-Show statt.</p>
-      
-      <a href="https://www.facebook.com/JasonBoonBigBand?fref=ts" target="_blank"><img src="images/facebook.png" width="150" align="right" /></a>
-      
+    <h1>Kampf der Orchester</h1>
+      <p>Die Jason Boon Bigband ist dabei beim <a href="http://www.srf.ch/kampfderorchester" target="_blank">Kampf der Orchester</a>!
+ </p>
+      <p><img src="images/boons_kdo.jpg" width="500" alt="JBBB" /> </p>
+      <p>Tixx für die Live-Shows in der Bodensee-Arena:<a href="https://www.starticket.ch/0showlist.asp?Language=D&txtSearch=kampf+der+orchester&suchen.x=0&suchen.y=0&fulltextHidden=1"> Starticket</a></p>
+      <div class="fb-like-box" data-href="https://www.facebook.com/JasonBoonBigband" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
     </div>
     
 
