380b08068fed3966ead3ef1442e74b31adee3376
Phantom75 <martin@schacher.ch>
2014-11-05 21:16:16 +0100

    rearrange iframes

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
       <p><img src="images/boons_kdo.jpg" width="500" alt="JBBB" /> </p>
       <p>Tixx für die Live-Shows in der Bodensee-Arena:<a href="https://www.starticket.ch/0showlist.asp?Language=D&txtSearch=kampf+der+orchester&suchen.x=0&suchen.y=0&fulltextHidden=1"> Starticket</a></p>
       
+      <iframe width="500" height="300" src="//www.youtube.com/embed/videoseries?list=PLilMLbHHTVTGODTlY90S9LsVLfi_a-ikB" frameborder="0" allowfullscreen></iframe>
+            
       <a class="twitter-timeline" width="200" height="200" href="https://twitter.com/JBBB_CH" data-widget-id="530088203810656256">Tweets von @JBBB_CH</a> 
       
       <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
       
-      <iframe width="500" height="300" src="//www.youtube.com/embed/videoseries?list=PLilMLbHHTVTGODTlY90S9LsVLfi_a-ikB" frameborder="0" allowfullscreen></iframe>
+
       
    </div>
     <div class="fb-like-box" data-href="https://www.facebook.com/jasonboonbigband" data-width="300" data-height="500" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="true" data-show-border="true"></div>
