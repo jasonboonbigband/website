71da89dee90bdd8a11fd78a36375fc495e46a33e
Icebell <ice-bell@gmx.ch>
2014-11-15 12:32:39 +0100

    galleryversuech

--- galerie.php
+++ galerie.php
@@ -1,5 +1,11 @@
 <?php include("header.php"); ?>
-
+	
+	
+	<!-- Start WOWSlider.com HEAD section --> <!-- add to the <head> of your page -->
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section -->
+    
 <div class ="mainborder">
 
   <div class ="header">
@@ -14,7 +20,120 @@
 
   <div class ="content">
     <h1> Galerie </h1>
-    <p>folgt.</p>
+    
+    
+    <!-- Start WOWSlider.com BODY section --> <!-- add to the <body> of your page -->
+	<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+		<li><img src="data1/images/img_6260.jpg" alt="IMG_6260" title="IMG_6260" id="wows1_0"/></li>
+		<li><img src="data1/images/img_6092.jpg" alt="IMG_6092" title="IMG_6092" id="wows1_1"/></li>
+		<li><img src="data1/images/img_6119.jpg" alt="IMG_6119" title="IMG_6119" id="wows1_2"/></li>
+		<li><img src="data1/images/img_6131.jpg" alt="IMG_6131" title="IMG_6131" id="wows1_3"/></li>
+		<li><img src="data1/images/img_6139.jpg" alt="IMG_6139" title="IMG_6139" id="wows1_4"/></li>
+		<li><img src="data1/images/img_6141.jpg" alt="IMG_6141" title="IMG_6141" id="wows1_5"/></li>
+		<li><img src="data1/images/img_6143.jpg" alt="IMG_6143" title="IMG_6143" id="wows1_6"/></li>
+		<li><img src="data1/images/img_6151.jpg" alt="IMG_6151" title="IMG_6151" id="wows1_7"/></li>
+		<li><img src="data1/images/img_6160.jpg" alt="IMG_6160" title="IMG_6160" id="wows1_8"/></li>
+		<li><img src="data1/images/img_6165.jpg" alt="IMG_6165" title="IMG_6165" id="wows1_9"/></li>
+		<li><img src="data1/images/img_6167.jpg" alt="IMG_6167" title="IMG_6167" id="wows1_10"/></li>
+		<li><img src="data1/images/img_6168.jpg" alt="IMG_6168" title="IMG_6168" id="wows1_11"/></li>
+		<li><img src="data1/images/img_6173.jpg" alt="IMG_6173" title="IMG_6173" id="wows1_12"/></li>
+		<li><img src="data1/images/img_6175.jpg" alt="IMG_6175" title="IMG_6175" id="wows1_13"/></li>
+		<li><img src="data1/images/img_6181.jpg" alt="IMG_6181" title="IMG_6181" id="wows1_14"/></li>
+		<li><img src="data1/images/img_6188.jpg" alt="IMG_6188" title="IMG_6188" id="wows1_15"/></li>
+		<li><img src="data1/images/img_6189.jpg" alt="IMG_6189" title="IMG_6189" id="wows1_16"/></li>
+		<li><img src="data1/images/img_6201.jpg" alt="IMG_6201" title="IMG_6201" id="wows1_17"/></li>
+		<li><img src="data1/images/img_6204.jpg" alt="IMG_6204" title="IMG_6204" id="wows1_18"/></li>
+		<li><img src="data1/images/img_6205.jpg" alt="IMG_6205" title="IMG_6205" id="wows1_19"/></li>
+		<li><img src="data1/images/img_6213.jpg" alt="IMG_6213" title="IMG_6213" id="wows1_20"/></li>
+		<li><img src="data1/images/img_6235.jpg" alt="IMG_6235" title="IMG_6235" id="wows1_21"/></li>
+		<li><img src="data1/images/img_6262.jpg" alt="IMG_6262" title="IMG_6262" id="wows1_22"/></li>
+		<li><img src="data1/images/img_6298.jpg" alt="IMG_6298" title="IMG_6298" id="wows1_23"/></li>
+		<li><img src="data1/images/img_6313.jpg" alt="IMG_6313" title="IMG_6313" id="wows1_24"/></li>
+		<li><img src="data1/images/img_6325.jpg" alt="IMG_6325" title="IMG_6325" id="wows1_25"/></li>
+		<li><img src="data1/images/img_6329.jpg" alt="IMG_6329" title="IMG_6329" id="wows1_26"/></li>
+		<li><img src="data1/images/img_6334.jpg" alt="IMG_6334" title="IMG_6334" id="wows1_27"/></li>
+		<li><img src="data1/images/img_6339.jpg" alt="IMG_6339" title="IMG_6339" id="wows1_28"/></li>
+		<li><img src="data1/images/img_6340.jpg" alt="IMG_6340" title="IMG_6340" id="wows1_29"/></li>
+		<li><img src="data1/images/img_6341.jpg" alt="IMG_6341" title="IMG_6341" id="wows1_30"/></li>
+		<li><img src="data1/images/img_6342.jpg" alt="IMG_6342" title="IMG_6342" id="wows1_31"/></li>
+		<li><img src="data1/images/img_6359.jpg" alt="IMG_6359" title="IMG_6359" id="wows1_32"/></li>
+		<li><img src="data1/images/img_6376.jpg" alt="IMG_6376" title="IMG_6376" id="wows1_33"/></li>
+		<li><img src="data1/images/img_6382.jpg" alt="IMG_6382" title="IMG_6382" id="wows1_34"/></li>
+		<li><img src="data1/images/img_6414.jpg" alt="IMG_6414" title="IMG_6414" id="wows1_35"/></li>
+		<li><img src="data1/images/img_6415.jpg" alt="IMG_6415" title="IMG_6415" id="wows1_36"/></li>
+		<li><img src="data1/images/img_6425.jpg" alt="IMG_6425" title="IMG_6425" id="wows1_37"/></li>
+		<li><img src="data1/images/img_6429.jpg" alt="IMG_6429" title="IMG_6429" id="wows1_38"/></li>
+		<li><img src="data1/images/img_6448.jpg" alt="IMG_6448" title="IMG_6448" id="wows1_39"/></li>
+		<li><img src="data1/images/img_6453.jpg" alt="IMG_6453" title="IMG_6453" id="wows1_40"/></li>
+		<li><img src="data1/images/img_6503.jpg" alt="IMG_6503" title="IMG_6503" id="wows1_41"/></li>
+		<li><img src="data1/images/img_6504.jpg" alt="IMG_6504" title="IMG_6504" id="wows1_42"/></li>
+		<li><img src="data1/images/img_6544.jpg" alt="IMG_6544" title="IMG_6544" id="wows1_43"/></li>
+		<li><img src="data1/images/img_6570.jpg" alt="IMG_6570" title="IMG_6570" id="wows1_44"/></li>
+		<li><img src="data1/images/img_6650.jpg" alt="IMG_6650" title="IMG_6650" id="wows1_45"/></li>
+		<li><img src="data1/images/img_6651.jpg" alt="IMG_6651" title="IMG_6651" id="wows1_46"/></li>
+		<li><a href="http://wowslider.com/vf"><img src="data1/images/img_6656.jpg" alt="full screen slider" title="IMG_6656" id="wows1_47"/></a></li>
+		<li><img src="data1/images/img_6663.jpg" alt="IMG_6663" title="IMG_6663" id="wows1_48"/></li>
+	</ul></div>
+	<div class="ws_thumbs">
+<div>
+		<a href="#" title="IMG_6260"><img src="data1/tooltips/img_6260.jpg" alt="" /></a>
+		<a href="#" title="IMG_6092"><img src="data1/tooltips/img_6092.jpg" alt="" /></a>
+		<a href="#" title="IMG_6119"><img src="data1/tooltips/img_6119.jpg" alt="" /></a>
+		<a href="#" title="IMG_6131"><img src="data1/tooltips/img_6131.jpg" alt="" /></a>
+		<a href="#" title="IMG_6139"><img src="data1/tooltips/img_6139.jpg" alt="" /></a>
+		<a href="#" title="IMG_6141"><img src="data1/tooltips/img_6141.jpg" alt="" /></a>
+		<a href="#" title="IMG_6143"><img src="data1/tooltips/img_6143.jpg" alt="" /></a>
+		<a href="#" title="IMG_6151"><img src="data1/tooltips/img_6151.jpg" alt="" /></a>
+		<a href="#" title="IMG_6160"><img src="data1/tooltips/img_6160.jpg" alt="" /></a>
+		<a href="#" title="IMG_6165"><img src="data1/tooltips/img_6165.jpg" alt="" /></a>
+		<a href="#" title="IMG_6167"><img src="data1/tooltips/img_6167.jpg" alt="" /></a>
+		<a href="#" title="IMG_6168"><img src="data1/tooltips/img_6168.jpg" alt="" /></a>
+		<a href="#" title="IMG_6173"><img src="data1/tooltips/img_6173.jpg" alt="" /></a>
+		<a href="#" title="IMG_6175"><img src="data1/tooltips/img_6175.jpg" alt="" /></a>
+		<a href="#" title="IMG_6181"><img src="data1/tooltips/img_6181.jpg" alt="" /></a>
+		<a href="#" title="IMG_6188"><img src="data1/tooltips/img_6188.jpg" alt="" /></a>
+		<a href="#" title="IMG_6189"><img src="data1/tooltips/img_6189.jpg" alt="" /></a>
+		<a href="#" title="IMG_6201"><img src="data1/tooltips/img_6201.jpg" alt="" /></a>
+		<a href="#" title="IMG_6204"><img src="data1/tooltips/img_6204.jpg" alt="" /></a>
+		<a href="#" title="IMG_6205"><img src="data1/tooltips/img_6205.jpg" alt="" /></a>
+		<a href="#" title="IMG_6213"><img src="data1/tooltips/img_6213.jpg" alt="" /></a>
+		<a href="#" title="IMG_6235"><img src="data1/tooltips/img_6235.jpg" alt="" /></a>
+		<a href="#" title="IMG_6262"><img src="data1/tooltips/img_6262.jpg" alt="" /></a>
+		<a href="#" title="IMG_6298"><img src="data1/tooltips/img_6298.jpg" alt="" /></a>
+		<a href="#" title="IMG_6313"><img src="data1/tooltips/img_6313.jpg" alt="" /></a>
+		<a href="#" title="IMG_6325"><img src="data1/tooltips/img_6325.jpg" alt="" /></a>
+		<a href="#" title="IMG_6329"><img src="data1/tooltips/img_6329.jpg" alt="" /></a>
+		<a href="#" title="IMG_6334"><img src="data1/tooltips/img_6334.jpg" alt="" /></a>
+		<a href="#" title="IMG_6339"><img src="data1/tooltips/img_6339.jpg" alt="" /></a>
+		<a href="#" title="IMG_6340"><img src="data1/tooltips/img_6340.jpg" alt="" /></a>
+		<a href="#" title="IMG_6341"><img src="data1/tooltips/img_6341.jpg" alt="" /></a>
+		<a href="#" title="IMG_6342"><img src="data1/tooltips/img_6342.jpg" alt="" /></a>
+		<a href="#" title="IMG_6359"><img src="data1/tooltips/img_6359.jpg" alt="" /></a>
+		<a href="#" title="IMG_6376"><img src="data1/tooltips/img_6376.jpg" alt="" /></a>
+		<a href="#" title="IMG_6382"><img src="data1/tooltips/img_6382.jpg" alt="" /></a>
+		<a href="#" title="IMG_6414"><img src="data1/tooltips/img_6414.jpg" alt="" /></a>
+		<a href="#" title="IMG_6415"><img src="data1/tooltips/img_6415.jpg" alt="" /></a>
+		<a href="#" title="IMG_6425"><img src="data1/tooltips/img_6425.jpg" alt="" /></a>
+		<a href="#" title="IMG_6429"><img src="data1/tooltips/img_6429.jpg" alt="" /></a>
+		<a href="#" title="IMG_6448"><img src="data1/tooltips/img_6448.jpg" alt="" /></a>
+		<a href="#" title="IMG_6453"><img src="data1/tooltips/img_6453.jpg" alt="" /></a>
+		<a href="#" title="IMG_6503"><img src="data1/tooltips/img_6503.jpg" alt="" /></a>
+		<a href="#" title="IMG_6504"><img src="data1/tooltips/img_6504.jpg" alt="" /></a>
+		<a href="#" title="IMG_6544"><img src="data1/tooltips/img_6544.jpg" alt="" /></a>
+		<a href="#" title="IMG_6570"><img src="data1/tooltips/img_6570.jpg" alt="" /></a>
+		<a href="#" title="IMG_6650"><img src="data1/tooltips/img_6650.jpg" alt="" /></a>
+		<a href="#" title="IMG_6651"><img src="data1/tooltips/img_6651.jpg" alt="" /></a>
+		<a href="#" title="IMG_6656"><img src="data1/tooltips/img_6656.jpg" alt="" /></a>
+		<a href="#" title="IMG_6663"><img src="data1/tooltips/img_6663.jpg" alt="" /></a>
+	</div>
+</div>
+<span class="wsl"><a href="http://wowslider.com/vu">image carousel</a> by WOWSlider.com v7.2</span>
+	<div class="ws_shadow"></div>
+	</div>	
+	<script type="text/javascript" src="engine1/wowslider.js"></script>
+	<script type="text/javascript" src="engine1/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
     <p>&nbsp;</p>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
