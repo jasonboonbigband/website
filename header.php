50222bd0c93c03f4648c087e476d772b15a54749
rehalbheer <33006011+rehalbheer@users.noreply.github.com>
2017-11-04 20:15:29 +0100

    Update header.php

--- header.php
+++ header.php
@@ -34,6 +34,12 @@
 
 <!-- End WOWSlider.com HEAD section -->
 	
+	<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine5/style.css" />
+<script type="text/javascript" src="engine5/jquery.js"></script>	<script src="https://www.youtube.com/iframe_api"></script>
+
+<!-- End WOWSlider.com HEAD section -->
+	
 	</head>
 
 <body>
