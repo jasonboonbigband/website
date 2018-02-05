75c4ff2187818741bddcd1d07caee8a27332c923
rehalbheer <33006011+rehalbheer@users.noreply.github.com>
2018-02-05 22:16:18 +0100

    Update galerie.php

--- galerie.php
+++ galerie.php
@@ -59,8 +59,8 @@
 <!-- End WOWSlider.com BODY section -->
 (Dank u.a. an den Klotener Anzeiger und die Unterland Zeitung für die Bilder)	  
 
-
-	  
+<br/>
+<br/>	  
 	  
 <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10 Jahre JBBB</h3>
 
