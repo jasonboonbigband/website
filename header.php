fca871e0ba106f6a2162b7f31213b76eca8feb0f
rehalbheer <33006011+rehalbheer@users.noreply.github.com>
2019-11-03 23:12:21 +0100

    Update header.php

--- header.php
+++ header.php
@@ -50,6 +50,12 @@
 <script type="text/javascript" src="engine18/jquery.js"></script>
 <!-- End WOWSlider.com HEAD section -->
 	
+	<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine20/style.css" />
+<script type="text/javascript" src="engine20/jquery.js"></script>	<script src="https://www.youtube.com/iframe_api"></script>
+
+<!-- End WOWSlider.com HEAD section -->
+	
 	</head>
 
 <body>
